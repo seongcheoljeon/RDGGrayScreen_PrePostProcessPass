4dc55261ffea612cff306be627914b56f69e2bdc
SeongcheolJeon <seongcheoljeon@icloud.com>
2026-01-14 02:14:07 +0900

    extension 작업중

--- Source/RDGGrayScreen_PrePostProcessPass/Private/RDGGrayScreenExtension.cpp
+++ Source/RDGGrayScreen_PrePostProcessPass/Private/RDGGrayScreenExtension.cpp
@@ -3,11 +3,56 @@
 
 #include "RDGGrayScreenExtension.h"
 
+#include "RDGGrayScreenLog.h"
+#include "RDGGrayScreenSubsystem.h"
 
-FRDGGrayScreenExtension::FRDGGrayScreenExtension()
+
+FRDGGrayScreenExtension::FRDGGrayScreenExtension(const FAutoRegister& auto_register, UWorld* in_world,
+                                                 URDGGrayScreenSubsystem* in_subsystem)
+		: FWorldSceneViewExtension(auto_register, in_world), _world_subsystem(in_subsystem)
+{
+	UE_LOG(LogRDGGrayScreen, Log, TEXT("GrayScreenExtension: Created! (FWorldSceneViewExtension"));
+}
+
+void FRDGGrayScreenExtension::BeginRenderViewFamily(FSceneViewFamily& InViewFamily)
+{
+	// game thread / render thread 양쪽에서 호출 가능
+	// editor viewport는 game thread에서 호출
+	// PIE/Game은 render thread에서 호출
+	if (::IsValid(_world_subsystem))
+	{
+		_world_subsystem->TransferStateToExtension(this);
+	}
+}
+
+// #TODO: 여기 작업해야 함.
+void FRDGGrayScreenExtension::PrePostProcessPass_RenderThread(FRDGBuilder& GraphBuilder, const FSceneView& InView,
+	const FPostProcessingInputs& Inputs)
 {
 }
 
-FRDGGrayScreenExtension::~FRDGGrayScreenExtension()
+bool FRDGGrayScreenExtension::IsActiveThisFrame_Internal(const FSceneViewExtensionContext& Context) const
 {
+	if (::IsValid(_world_subsystem))
+	{
+		// gray_screen이 0이 아닐때만 활성화되도록
+		return _world_subsystem->ShouldBeActiveThisFrame();
+	}
+	return false;
+}
+
+void FRDGGrayScreenExtension::SetParameters(float in_gray_screen, float in_gray_gamma)
+{
+	check(::IsInRenderingThread());
+	FScopeLock lock_(&_render_parameter_cs);
+	_renderthread_gray_screen = in_gray_screen;
+	_renderthread_gray_gamma = in_gray_gamma;
+}
+
+void FRDGGrayScreenExtension::GetParameters(float& out_gray_screen, float& out_gray_gamma) const
+{
+	check(IsInRenderingThread());
+	FScopeLock lock_(&_render_parameter_cs);
+	out_gray_screen = _renderthread_gray_screen;
+	out_gray_gamma = _renderthread_gray_gamma;
 }
--- Source/RDGGrayScreen_PrePostProcessPass/Public/RDGGrayScreenExtension.h
+++ Source/RDGGrayScreen_PrePostProcessPass/Public/RDGGrayScreenExtension.h
@@ -5,15 +5,44 @@
 #include "CoreMinimal.h"
 #include "SceneViewExtension.h"
 
+class URDGGrayScreenSubsystem;
+
 /**
- * 
+ * FworldSceneViewExtension 상속:
+ * - world 생명주기와 자동 연결
+ * - world 파괴 시, 자동 정리
+ * game thread와 render thread 데이터 분리
  */
+
 class FRDGGrayScreenExtension : public FWorldSceneViewExtension
 {
 public:
-	FRDGGrayScreenExtension();
-	virtual ~FRDGGrayScreenExtension();
+	FRDGGrayScreenExtension(const FAutoRegister& auto_register, UWorld* in_world, URDGGrayScreenSubsystem* in_subsystem);;
+	virtual ~FRDGGrayScreenExtension() = default;
 	
 public:
+	virtual void SetupViewFamily(FSceneViewFamily& InViewFamily) override {};
+	virtual void SetupView(FSceneViewFamily& InViewFamily, FSceneView& InView) override {};
+	virtual void BeginRenderViewFamily(FSceneViewFamily& InViewFamily) override;
+	virtual void PrePostProcessPass_RenderThread(FRDGBuilder& GraphBuilder, const FSceneView& InView,
+		const FPostProcessingInputs& Inputs) override;
+
+protected:
+	virtual bool IsActiveThisFrame_Internal(const FSceneViewExtensionContext& Context) const override;
+	
+public:
+	// render thread: 파라미터 설정
 	void SetParameters(float in_gray_screen, float in_gray_gamma);
+	// render thread: 파라미터 읽기
+	void GetParameters(float& out_gray_screen, float& out_gray_gamma) const;
+	
+private:
+	// 0: original, 1: full grayscale screen
+	float _renderthread_gray_screen = 0.0;
+	// 0: black, 1: normal
+	float _renderthread_gray_gamma = 1.0;
+	// critical section
+	mutable FCriticalSection _render_parameter_cs;
+	// subsystem reference (IsActiveThisFrame 위임용)
+	URDGGrayScreenSubsystem* _world_subsystem;
 };
